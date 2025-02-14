764868afd5da8e3f3e8ee519eb2459d91683ba57
nakawodayo <dev.knakao@gmail.com>
2025-02-14 12:34:11 +0900

    ビルド設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// リポジトリ名を `base` に設定
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/meeting-timer/",
+});
